eb8a84e62cda301ef673be4968ac4c4c4e2af75c
Cristian Gomez Peral <demoncriss@hotmail.com>
2013-10-11 09:57:43 +0200

    Cambio del metodo detect con underscore

--- 07-gameboard/engine.js
+++ 07-gameboard/engine.js
@@ -221,10 +221,11 @@ var GameBoard = function() {
 
     // Devuelve el primer objeto de objects para el que func es true
     this.detect = function(func) {
-	for(var i = 0,val=null, len=this.objects.length; i < len; i++) {
+	/*for(var i = 0,val=null, len=this.objects.length; i < len; i++) {
 	    if(func.call(this.objects[i])) return this.objects[i];
 	}
-	return false;
+	return false;*/
+	var first = _.find(this.objects, function(func){ return func });
     };
 
     // Cuando Game.loop() llame a step(), hay que llamar al método
